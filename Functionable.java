9f8edb7586c47fa3bc80f0d4d399c6763e4aea8b
khatchapuridzeluka <khatchapuridzeluka91@gmail.com>
2024-12-27 02:13:27 +0400

    Update Functionable.java

--- Functionable.java
+++ Functionable.java
@@ -1,9 +1,11 @@
 package interpreter;
 
 import java.util.HashMap;
+import java.util.Set;
 
 public interface Functionable {
 	void setValue(String variable, int value, HashMap<String,Integer> map);
 	void print(String name, HashMap<String,Integer> map);
+	void variableAssignment(String[] code, int i, boolean isVarDeclaration, Set<Character> operations, HashMap<String,Integer> map);
 	int calculate(char operand, String s, int sum, HashMap<String,Integer> map);
 }
