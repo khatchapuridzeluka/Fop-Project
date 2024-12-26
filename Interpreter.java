106f48179bb2d315d7eeae77bc1742570e36c3a0
khatchapuridzeluka <khatchapuridzeluka91@gmail.com>
2024-12-27 01:42:38 +0400

    Update Interpreter.java

--- Interpreter.java
+++ Interpreter.java
@@ -7,59 +7,50 @@ import java.util.Set;
 import java.io.IOException;
 
 public class Interpreter {
-	private String url;
-	private HashMap<String, Integer> map = new HashMap<>();
-	Functions functions = new Functions();
-	private Set<Character> operations = Set.of('+', '-', '%','/', '*');
-	public Interpreter(String url) {
-		this.url = url;
-	}
-	
-	// reading the .txt file
-	public void readSwift() {
-		String content = "";
-	    try {
-	    	content = Files.readString(Paths.get(url));
-	    } catch (IOException e) {
-	        System.out.println("An error occurred: " + e.getMessage());
-	    }
-	    String[] code = content.split("\\s+");
-	    
-	    compile(code);
-	   
-	}
-	
-	// executring the code 
-	private void compile(String[] code) {		
-		for(int i =0; i < code.length; i ++) {
-			// inisializing the variable
-			if(code[i].equals(Keys.var.name())) {
-				int index = i+1;
-				int sum = 0;
-				if(Character.isDigit(code[i+3].charAt(0))) {
-					sum = Integer.valueOf(code[i+3]);
-				}else {
-					sum+=map.get(code[i+3]);
-				}
-				i = i+4;
-				while(operations.contains(code[i].charAt(0))) {
-					sum = functions.calculate(code[i].charAt(0), code[i+1], sum, map);
-					i=i+2;
-				}
-				functions.setValue(code[index],sum, map);
-				i = i-1;
-			}
-			// needs to be repaired ( ugly code ) 
-			// printing the variable
-			if(code[i].startsWith(Keys.print.name())) {
-				String varName = "";
-				int j = 6;
-				while(code[i].charAt(j) !=')') {
-					varName+=code[i].charAt(j);
-					j++;
-				}
-				functions.print(varName, map);
-			}
-	}
-	}
+    private String url;
+    private HashMap<String, Integer> map = new HashMap<>();
+    private Functions functions = new Functions();
+    private Set<Character> operations = Set.of('+', '-', '%', '/', '*');
+
+    public Interpreter(String url) {
+        this.url = url;
+    }
+
+    // Reading the .txt file and compiling
+    public void readSwift() {
+        try {
+            String content = Files.readString(Paths.get(url));
+            compile(content.split("\\s+"));
+        } catch (IOException e) {
+            System.out.println("An error occurred: " + e.getMessage());
+        }
+    }
+
+    // Compiling and executing the code
+    private void compile(String[] code) {
+        for (int i = 0; i < code.length; i++) {
+            // Initializing variables
+            if (code[i].equals(Keys.var.name())) {
+                String variable = code[++i];
+                int sum = Character.isDigit(code[i + 2].charAt(0))
+                        ? Integer.parseInt(code[i + 2])
+                        : map.get(code[i + 2]);
+                i += 3;
+
+                // Process operations
+                while (operations.contains(code[i].charAt(0))) {
+                    sum = functions.calculate(code[i].charAt(0), code[++i], sum, map);
+                    i++;
+                }
+                functions.setValue(variable, sum, map);
+                i--;
+            }
+
+            // Printing variables
+            if (code[i].startsWith(Keys.print.name())) {
+                String varName = code[i].substring(6, code[i].length() - 1); // Extract variable name
+                functions.print(varName, map);
+            }
+        }
+    }
 }
