732fdea58dd2c851164fb2f86bae9c945ef69004
khatchapuridzeluka <khatchapuridzeluka91@gmail.com>
2024-12-28 00:14:15 +0400

    Update Keys.java
    The structure of the KEYS have been changed

--- Keys.java
+++ Keys.java
@@ -3,7 +3,33 @@ package interpreter;
 // Any new Key word from Swift can be added here
 
 public enum Keys {
-	var,
-	print,
-	IF
+	VAR {
+		@Override
+		public String toString() {
+			return "var";
+		}
+	},
+	PRINT {
+		@Override
+		public String toString() {
+			return "print";
+		}
+	},
+	IF {
+		@Override
+		public String toString() {
+			return "if";
+		}
+	},
+	
+	WHILE {
+		@Override
+		public String toString() {
+			return "while";
+		}
+	};
+	
+	
+	
+	public abstract String toString();
 }
