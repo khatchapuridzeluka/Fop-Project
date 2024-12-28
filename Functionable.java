16e2e16076eed1bf23f7d49d6e4b2ff1293fe8a4
khatchapuridzeluka <khatchapuridzeluka91@gmail.com>
2024-12-28 22:59:12 +0400

    Update Functionable.java
    added some more methods

--- Functionable.java
+++ Functionable.java
@@ -1,17 +1,24 @@
 package interpreter;
 
-import java.util.HashMap;
 import java.util.Set;
 
 public interface Functionable {
 	
-	void setValue(String variable, int value, HashMap<String,Integer> map);
+	void setValue(String variable, int value);
 	
-	void print(String name, HashMap<String,Integer> map, boolean isVar);
+	void print(String name, boolean isVar);
 	
-	int calculate(char operand, String s, int sum, HashMap<String,Integer> map);
+	int handlePrint(String[] code, int i);
 	
-	void variableAssignment(String[] code, int i, boolean isVarDeclaration, Set<Character> operations, HashMap<String,Integer> map);
+	int calculate(char operand, String s, int sum);
+	
+	void variableAssignment(String[] code, int i, boolean isVarDeclaration, Set<Character> operations);
 	
 	boolean isTrue(int sum, int otherSum, char statement);
+	
+	int handleIf(String[] code, int i, Set<Character> statements);
+	
+	int handleWhile(String[] code, int i);
+	
+	int handleIsWhile(String[] code, int i);
 }
