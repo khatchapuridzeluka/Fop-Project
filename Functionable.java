338c9fbe17792fd078a98443df3a86393a50684c
khatchapuridzeluka <khatchapuridzeluka91@gmail.com>
2024-12-29 22:52:40 +0400

    Update Functionable.java
    comments have been added

--- Functionable.java
+++ Functionable.java
@@ -4,25 +4,37 @@ import java.util.Set;
 
 public interface Functionable {
 	
+	// setting the value to the variable 
 	void setValue(String variable, int value);
 	
-	void print(String name, boolean isVar);
-	
-	int handlePrint(String[] code, int i);
-	
-	int calculate(char operand, String s, int sum);
-	
+	// assigning the variable
 	void variableAssignment(String[] code, int i, boolean isVarDeclaration, Set<Character> operations);
-	
-	boolean isTrue(int sum, int otherSum, char statement);
-	
-	int handleIf(String[] code, int i, Set<Character> statements);
-	
-	int handleWhile(String[] code, int i);
-	
-	int handleIsWhile(String[] code, int i);
-	
+
+	// handling ++ and --
 	void handleIncDec(String string);
 	
+	// printing the variable or just text
+	void print(String name, boolean isVar);
+	
+	// handling print function
+	int handlePrint(String[] code, int i);
+	
+	// calculating arithmetic operations
+	int calculate(char operand, String s, int sum);
+	
+	// checks if the condition is true
+	boolean isTrue(int sum, int otherSum, char statement);
+	
+	// handling if statement
+	int handleIf(String[] code, int i, Set<Character> statements);
+
+	// handling else statment
 	int handleELSE(String[] code, int i);
+	
+	// handling while loop
+	int handleWhile(String[] code, int i);
+	
+	// checks if there is while going on
+	int handleIsWhile(String[] code, int i);
+	
 }
